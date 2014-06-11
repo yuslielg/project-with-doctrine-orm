807f621b2e2f09be4943ccbdf1905e55e61455d8
Yusliel Garcia <yuslielg@gmail.com>
2014-06-11 09:07:44 -0400

    Created directory structure

---
+++ src/Contrask/Component/Project/Entity/Project.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Contrask\Component\Project\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @author Yusliel Garcia <yuslielg@gmail.com>
+ * @ORM\Entity
+ */
+class Project implements MessageInterface 
+{
+    /**
+     * @var integer
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
---
+++ src/Contrask/Component/Project/Manager/ProjectManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Contrask\Component\Project\Manager;
+
+use Contrask\Component\Project\Manager\ProjectManagerInterface
+
+/**
+ * @author Yusliel Garcia <yuslielg@gmail.com>
+ */
+class ProjectManager implements ProjectManagerInterface
+{
+   
+}
